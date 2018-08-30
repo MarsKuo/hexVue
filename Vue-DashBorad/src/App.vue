72ebc5ba34b7072f72505d682989d7dbcd326872
Mars <talent.seek@msa.hinet.net>
2018-08-30 18:04:42 +0800

    20180830

--- Vue-DashBorad/src/App.vue
+++ Vue-DashBorad/src/App.vue
@@ -6,19 +6,11 @@
 
 <script>
 export default {
-  name: 'App',
-  created() {
-    // const api = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}'`;
-    // console.log(api);
-    // this.$http.get(api).then(response => {
-    //   console.log(response.data);
-    // });
-  }
+  name: 'App'
 };
 </script>
 
 <style lang="scss">
 // @import "~bootstrap/scss/bootstrap";
-@import "./assets/all"
-
+@import './assets/all';
 </style>
